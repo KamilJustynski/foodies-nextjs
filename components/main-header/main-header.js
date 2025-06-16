fa80091103d01fd47dc4335cede8e9795a5c6ef5
Kamil Justynski <kamil.justynski1998@gmail.com>
2025-06-16 14:54:48 +0200

    feature: enhance CommunityPage layout and styles; add NavLink component for navigation

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,8 +1,10 @@
+
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "@/components/main-header/main-header-background";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
   return (
@@ -17,10 +19,10 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
