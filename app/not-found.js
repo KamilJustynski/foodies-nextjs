535d7e96bb5dfefec37377dd20186284f1065f3f
Kamil Justynski <kamil.justynski1998@gmail.com>
2025-06-20 12:39:05 +0200

    feature: add NotFound component for handling 404 errors

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <main class="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately, the page you were looking for could not be found.</p>
+    </main>
+  );
+};
+export default NotFound;
