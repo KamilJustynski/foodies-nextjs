5f2f47dbef50cc4700c2b32ed1f20f38c42ab41a
Kamil Justynski <kamil.justynski1998@gmail.com>
2025-06-20 12:20:07 +0200

    feature: ddd meal components and database initialization
    - Created CSS styles for meal items in meal-item.module.css.
    - Implemented MealsGrid component to display a grid of meals in meals-grid.js.
    - Added CSS styles for the meals grid in meals-grid.module.css.
    - Initialized a SQLite database with dummy meal data in initdb.js.
    - Created a function to fetch meals from the database in lib/meals.js.
    - Added better-sqlite3 as a dependency in package.json and package-lock.json.
    - Created a new SQLite database file meals.db.

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,12 +1,36 @@
 import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import Loading from "./loading-out";
+
+const Meals = async () => {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
+
 const MealsPage = () => {
   return (
-    <div>
-      Meals Page
-      <Link href={`/meals/blog-1`}>Blog 1</Link>
-      <Link href={`/meals/blog-2`}>Blog 2</Link>
-      <Link href={`/meals/blog-3`}>Blog 3</Link>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<Loading />}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
   );
 };
 
