84750d910b79b7295baf4f203a578221ef046c06
Kamil Justynski <kamil.justynski1998@gmail.com>
2025-06-29 14:37:55 +0200

    feature: implement MealDetailPage with meal fetching and styling; add NotFound component for 404 handling

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,38 @@
-import React from "react";
-
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 const MealDetailPage = ({ params }) => {
-  return <div>Meal Detail Page: {params.slug}</div>;
+  const meal = getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
 export default MealDetailPage;
